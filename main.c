5860f9d69aeb7d65ecc990530ce0954e88d4ff5e
chaeyoung0813 <chaeyoung0813@naver.com>
2022-11-15 16:18:27 +0900

    01

--- main.c
+++ main.c
@@ -9,9 +9,13 @@ char c = 'a';
 int *iptr;
 char *cptr;
 int *iptr2;
+
 iptr = &i;
 cptr = &c;
+
 iptr2 = &i;
+
+
 printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
 printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
 printf("iptr2 : %p, %i\n", iptr2, *iptr2);
