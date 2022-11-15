093f4027fe075ac0aef35cdd5372acef998b8e5b
chaeyoung0813 <chaeyoung0813@naver.com>
2022-11-15 16:52:46 +0900

    05

--- main.c
+++ main.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/**
-void swap (int x, int y) {
-int temp;
-temp = x;
-x = y;
-y = temp;
+int main(void)
+{
+int i;
+int accum = 0;
+int grade[5];
+for (i=0;i<5;i++)
+{
+printf("grade[%i] = ", i);
+scanf("%d", &grade[i]);
 }
-void main(void) {
-int a = 3;
-int b = 5;
-swap(a, b);
-printf("a:%i, b:%i\n", a, b);
+for (i=0; i<5; i++)
+{
+	printf("%i : %i\n", i, *(grade+i));
+	accum += *(grade+i);
 }
-**/
-void swap (int *x, int *y) {
-int temp;
-temp = *x;
-*x = *y;
-*y = temp;
-}
-void main(void) {
-int a = 3;
-int b = 5;
-swap(&a, &b);
-printf("a%i, b%i\n", a, b);
+return 0;
 }
